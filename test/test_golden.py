6e00ab20a7f7e3265e1b444b31a749e4b5b51fc2
Bill Fisher <william.w.fisher@gmail.com>
2016-09-28 15:55:28 -0700

    Make a test case that tests example.txt via unittest

---
+++ test/test_golden.py
@@ -0,0 +1,61 @@
+import unittest
+import precis_codec
+import os
+
+
+GOLDEN = os.path.join(os.path.dirname(__file__), 'examples.txt')
+
+
+class TestGolden(unittest.TestCase):
+
+    def test_golden_file(self):
+        for profile, allow, input, expected in _read_golden():
+            if allow:
+                self.check_allow(profile, input, expected)
+            else:
+                self.check_disallow(profile, input, expected)
+
+    def check_allow(self, profile, input, expected):
+        #print('check_allow', profile, input)
+        actual = input.encode(profile).decode('utf-8')
+        self.assertEqual(actual, expected)
+        idempotent = actual.encode(profile).decode('utf-8')
+        self.assertEqual(idempotent, actual)
+
+    def check_disallow(self, profile, input, expected):
+        #print('check_disallow', profile, input)
+        with self.assertRaisesRegex(UnicodeEncodeError, expected):
+            input.encode(profile)
+
+
+
+def _read_golden():
+    """ Generator function to return 4-tuples with test data:
+    
+        (profile, allow, input, expected)
+
+    e.g. ('UsernameCaseMapped', True, 'Kevin', 'kevin')
+    """
+    with open(GOLDEN, encoding='utf-8') as golden:
+        for line in golden:
+            fields = [_unescape(s) for s in line.strip().split()]
+            if len(fields) == 0 or fields[0].startswith('#'):
+                continue
+
+            cmd = fields[0].upper()
+            if cmd == 'PROFILE':
+                profile = fields[1]
+            elif cmd == 'ALLOW':
+                if len(fields) == 2:
+                    fields.append(fields[1])
+                yield (profile, True, fields[1], fields[2])
+            elif cmd == 'DISALLOW':
+                yield (profile, False, fields[1], fields[2])
+
+
+def _unescape(s):
+    return s.encode('raw-unicode-escape').decode('unicode_escape')
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
