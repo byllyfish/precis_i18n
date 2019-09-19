e4fa4491395c0b8c7a1fd50b557b928b713eade7
Bill Fisher <william.w.fisher@gmail.com>
2019-09-19 12:54:46 -0700

    Add test that uses unicodedata2 and unicode 12.0.

--- test/test_derived_props.py
+++ test/test_derived_props.py
@@ -4,21 +4,36 @@ import sys
 from precis_i18n.derived import derived_property
 from precis_i18n.unicode import UnicodeData
 
-UCD = UnicodeData()
-GOLDNAME = 'derived-props-%.1f.txt' % UCD.version
-GOLDPATH = os.path.join(os.path.dirname(__file__), GOLDNAME)
-
 
 class TestDerivedProperties(unittest.TestCase):
     """ Test output of derived_property function.
     """
 
     def test_derived_props(self):
+        """ Compare derived properties against a "golden" file using built-in
+        unicodedata module.
+        """
+        self._test_derived_props(UnicodeData())
+
+    def test_derived_props_unicodedata2(self):
+        """ Compare derived properties against a "golden" file using
+        pip-installed unicodedata2 module.
+        """
+
+        import unicodedata2
+        ucd = UnicodeData(unicodedata2)
+        assert ucd.version == 12.0
+        self._test_derived_props(ucd)
+
+    def _test_derived_props(self, ucd):
         """ Compare derived properties against a "golden" file.
         """
 
-        with open(GOLDPATH) as golden:
-            derived_props = enumerate_derived_props()
+        goldname = 'derived-props-%.1f.txt' % ucd.version
+        goldpath = os.path.join(os.path.dirname(__file__), goldname)
+
+        with open(goldpath) as golden:
+            derived_props = enumerate_derived_props(ucd)
             for line in golden:
                 line = line.strip()
                 if not line:
@@ -27,15 +42,15 @@ class TestDerivedProperties(unittest.TestCase):
                 self.assertEqual(line, expected)
 
 
-def enumerate_derived_props(begin=0, end=0x00110000):
+def enumerate_derived_props(ucd, begin=0, end=0x00110000):
     """ Iterable producing all derived_property values for a particular range.
 
     Produces a sequence of tuples (begin, end, property).
     """
     start = begin
-    prev = '/'.join(derived_property(start, UCD))
+    prev = '/'.join(derived_property(start, ucd))
     for cp in range(start + 1, end):
-        prop = '/'.join(derived_property(cp, UCD))
+        prop = '/'.join(derived_property(cp, ucd))
         if prop != prev:
             yield (start, cp - 1, prev)
             start = cp
@@ -43,8 +58,13 @@ def enumerate_derived_props(begin=0, end=0x00110000):
     yield (start, cp, prev)
 
 
-if __name__ == '__main__':
-    print('Unicode %.1f' % UCD.version, file=sys.stderr)
+def main():
+    ucd = UnicodeData()
+    print('Unicode %.1f' % ucd.version, file=sys.stderr)
     # Dump gold file to stdout.
-    for p in enumerate_derived_props():
+    for p in enumerate_derived_props(ucd):
         print('%04X-%04X %s' % p)
+
+
+if __name__ == '__main__':
+    main()
