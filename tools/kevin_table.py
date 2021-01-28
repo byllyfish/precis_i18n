39bfbcc4704b53258ff1da5c4782397cd572298b
Bill Fisher <william.w.fisher@gmail.com>
2021-01-28 12:42:50 -0700

    Formatting fixes.

--- tools/kevin_table.py
+++ tools/kevin_table.py
@@ -2,14 +2,18 @@
 Generate 'Kevin' table for README.
 """
 
-import precis_i18n.codec
+import precis_i18n.codec  # noqa: F401
 
+letter_k = [
+    ord('K'), 0x212A, 0xFF2B, 0x039A, 0x1E32, 0x1E34, 0x2C69, 0xA740, 0xA742,
+    0xA744, 0xA7A2, 0x24C0, 0x1F11A, 0x1F13A, 0x1F15A, 0x1F17A
+]
 
-letter_k = [ ord('K'), 0x212A, 0xFF2B, 0x039A, 0x1E32, 0x1E34, 0x2C69, 0xA740, 
-             0xA742, 0xA744, 0xA7A2, 0x24C0, 0x1F11A, 0x1F13A, 0x1F15A, 0x1F17A]
 
 def _escape(s):
-    return s.encode('raw-unicode-escape').decode('ascii').replace('\\', '&#92;')
+    return s.encode('raw-unicode-escape').decode('ascii').replace(
+        '\\', '&#92;')
+
 
 def _xml_escape(s):
     return s.encode('ascii', errors='xmlcharrefreplace').decode('ascii')
@@ -25,9 +29,12 @@ def _column(s):
     return "%s (%s)" % (_xml_escape(s), _escape(s))
 
 
-
-print('Original String|UsernameCasePreserved|UsernameCaseMapped|NicknameCaseMapped')
-print('---------------|---------------------|------------------|------------------')
+print(
+    'Original String|UsernameCasePreserved|UsernameCaseMapped|NicknameCaseMapped'
+)
+print(
+    '---------------|---------------------|------------------|------------------'
+)
 
 for k in letter_k:
     kevin = chr(k) + 'evin'
@@ -50,4 +57,3 @@ for k in letter_k:
     col4 = _column(nickname)
 
     print('%s | %s | %s | %s' % (col1, col2, col3, col4))
-
