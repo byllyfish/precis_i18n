0c3714958c65d8ba004b9e874db4471c344f088c
Bill Fisher <william.w.fisher@gmail.com>
2022-01-17 18:18:31 -0700

    Bump version and update copyright year.

--- precis_i18n/__init__.py
+++ precis_i18n/__init__.py
@@ -1,5 +1,5 @@
 """PRECIS-i18n: Internationalized Usernames and Passwords."""
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 from precis_i18n.factory import get_profile as get_profile  # noqa: F401
