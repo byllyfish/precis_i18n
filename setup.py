413a79d6be209fba446092b47b03db4299ee3e2e
Bill Fisher <william.w.fisher@gmail.com>
2024-11-12 09:32:40 -0700

    Add Python 3.13 to setup.py classifiers.

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Software Development :: Internationalization",
     ],
     zip_safe=True,
